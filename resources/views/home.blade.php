bfcbe2e903381d158df6eb8d6ed9e4fc7ba774f8
shini <matteoezio@me.com>
2021-02-22 17:06:07 +0100

    completo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('content')
+        <div class="flex-center position-ref full-height">
+
+            <div class="content">
+
+                <nav class="links">
+                    <a href="/" >Home</a>
+                    <a href="about">About</a>
+                    <a href="blog">Blog</a>
+                </nav>
+
+                <div class="title m-b-md">
+                    Home
+                </div>
+            </div>
+        </div>
+@endsection
